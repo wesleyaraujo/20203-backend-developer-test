0c85bbf9e733f1b43fba1a3c6f37c271c64c2de7
Rafael Nolêto <rafael.noleto@bling.com.br>
2020-10-23 03:04:52 -0300

    finalizado post de sobreviventes;

---
+++ survivors/src/main/java/br/dev/rafaelnoleto/survivors/model/dao/SurvivorItemDao.java
@@ -0,0 +1,37 @@
+package br.dev.rafaelnoleto.survivors.model.dao;
+
+import br.dev.rafaelnoleto.survivors.model.entity.SurvivorEntity;
+import java.util.List;
+
+/**
+ *
+ * @author rafaelnoleto
+ */
+public class SurvivorDao implements Dao<SurvivorEntity> {
+
+    @Override
+    public Integer create(SurvivorEntity survivorEntity) {
+        return null;
+    }
+
+    @Override
+    public SurvivorEntity readOne() {
+        return null;
+    }
+
+    @Override
+    public List<SurvivorEntity> readAll() {
+        return null;
+    }
+
+    @Override
+    public Boolean update(Integer id) {
+        return null;
+    }
+
+    @Override
+    public Boolean delete(Integer id) {
+        return null;
+    }
+
+}
