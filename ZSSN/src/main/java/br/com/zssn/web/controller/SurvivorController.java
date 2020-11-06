57cef4d738ffca16e2ae03df783a405bf2f661ce
wesleyaraujo <wesleyaraujopi@gmail.com>
2020-11-06 08:31:14 -0300

    Adiciondados método createSurvivor e updateLocation e removido método não necessário.

--- ZSSN/src/main/java/br/com/zssn/web/controller/SurvivorController.java
+++ ZSSN/src/main/java/br/com/zssn/web/controller/SurvivorController.java
@@ -1,12 +1,21 @@
 package br.com.zssn.web.controller;
 
 import java.util.List;
+import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.zssn.entity.Position;
 import br.com.zssn.entity.Survivor;
 import br.com.zssn.service.SurvivorService;
 import lombok.extern.slf4j.Slf4j;
@@ -23,10 +32,22 @@ public class SurvivorController {
 		this.survivorService = survivorService;
 	}
 
-	@GetMapping("")
-	public List<Survivor> getUsers() {
-		log.info("process=get-survivors");
-		return survivorService.getAllSurvivors();
+	@PostMapping("")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Survivor createSurvivor(@RequestBody Survivor survivor) {
+		log.info("process=create-survivor, name={}", survivor.getName());
+		return survivorService.createSurvivor(survivor);
+	}
+
+	@PutMapping("/{id}")
+	@ResponseStatus(code = HttpStatus.OK)
+	public ResponseEntity<Survivor> updateLocation(@PathVariable Long id, @RequestBody Position newPosition) {
+		Survivor updateLocation = survivorService.updateLocation(id, newPosition);
+		if (Objects.nonNull(updateLocation)) {
+			return ResponseEntity.ok(updateLocation);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
 	}
 
 }
