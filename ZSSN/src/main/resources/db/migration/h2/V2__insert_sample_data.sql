612ece4017b75377730157598bcd62dd55ec915b
wesleyaraujo <wesleyaraujopi@gmail.com>
2020-11-06 08:32:20 -0300

    Adicionado script para inserir os resources

--- ZSSN/src/main/resources/db/migration/h2/V2__insert_sample_data.sql
+++ ZSSN/src/main/resources/db/migration/h2/V2__insert_sample_data.sql
@@ -6,3 +6,10 @@ INSERT INTO users (email, name, created_at) VALUES
 
 INSERT INTO survivors (name, birthdate, gender, last_latitude, last_longitude) VALUES
 ('Wesley',to_date('1989-08-14','yyyy-MM-dd'), 'M', -5.08921, -42.8016);
+
+INSERT INTO resources (name,point) VALUES
+('Water',4),
+('Food',3),
+('Medication',2),
+('Ammunition',1)
+;
