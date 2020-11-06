c8e4bdc0c353500b4460d6ab9c9e2bcdb06e396b
wesleyaraujo <wesleyaraujopi@gmail.com>
2020-11-06 08:31:50 -0300

    Adicionado script para criação da sequencia e da tabela de resources

--- ZSSN/src/main/resources/db/migration/h2/V1__create_tables.sql
+++ ZSSN/src/main/resources/db/migration/h2/V1__create_tables.sql
@@ -10,7 +10,7 @@ create table users (
     UNIQUE KEY user_email_unique (email)
 );
 
-create sequence survivor_id_seq start with 1 increment by 1;
+create sequence survivor_id_seq start with 10 increment by 50;
 
 create table survivors (
 	id bigint DEFAULT survivor_id_seq.nextval,
@@ -21,5 +21,14 @@ create table survivors (
 	gender varchar(1),
 	created_at timestamp,
     updated_at timestamp,
+	primary key (id)
+);
+
+create sequence resources_id_seq start with 10 increment by 50;
+
+create table resources (
+	id bigint DEFAULT resources_id_seq.nextval,
+	name varchar(255) not null,
+	point float(2) not null,
 	primary key (id)
 );
--- ZSSN/src/main/resources/db/migration/postgresql/V1__create_tables.sql
+++ ZSSN/src/main/resources/db/migration/postgresql/V1__create_tables.sql
@@ -1,4 +1,4 @@
-create sequence user_id_seq start with 10 increment by 50;
+create sequence user_id_seq start with 1 increment by 1;
 
 create table users (
     id bigint DEFAULT nextval('user_id_seq') not null,
@@ -11,14 +11,23 @@ create table users (
 
 create sequence survivor_id_seq start with 1 increment by 1;
 
-create table survivors(
+create table survivors (
 	id bigint DEFAULT nextval('survivor_id_seq') not null,
 	name varchar(255) not null,
 	birthdate timestamp,
-	last_latitude float(9,6),
-	last_longitude float(9,6),
+	last_latitude numeric(8,4),
+	last_longitude numeric(8,4),
 	gender varchar(1),
 	created_at timestamp,
     updated_at timestamp,
+	primary key (id)
+);
+
+create sequence resources_id_seq start with 1 increment by 1;
+
+create table resources (
+	id bigint DEFAULT nextval('resources_id_seq'),
+	name varchar(255) not null,
+	point numeric(4,2) not null,
 	primary key (id)
 );
