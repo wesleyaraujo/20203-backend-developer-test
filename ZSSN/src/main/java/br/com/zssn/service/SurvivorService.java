f51216767e1785495701cb7e1b430b64568fa0a5
wesleyaraujo <wesleyaraujopi@gmail.com>
2020-11-06 08:32:34 -0300

    Adicionado método updateLocation

--- ZSSN/src/main/java/br/com/zssn/service/SurvivorService.java
+++ ZSSN/src/main/java/br/com/zssn/service/SurvivorService.java
@@ -1,11 +1,13 @@
 package br.com.zssn.service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import br.com.zssn.entity.Position;
 import br.com.zssn.entity.Survivor;
 import br.com.zssn.repo.SurvivorRepository;
 
@@ -42,6 +44,19 @@ public class SurvivorService {
 
 	public void deleteSurvivor(Long survivorId) {
 		survivorRepository.deleteById(survivorId);
+	}
+
+	public Survivor updateLocation(Long survivorId, Position newPosition) {
+		if (Objects.nonNull(survivorId)) {
+			Optional<Survivor> survivorDB = survivorRepository.findById(survivorId);
+			Survivor survivor = survivorDB.map(u -> u).orElse(null);
+			if (Objects.nonNull(survivor)) {
+				survivor.setLastPosition(newPosition);
+				survivorRepository.save(survivor);
+				return survivor;
+			}
+		}
+		return null;
 	}
 
 }
