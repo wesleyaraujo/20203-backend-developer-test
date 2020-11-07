08c951f73c2a82160c5205989191a02a76f150ec
Wesley Araujo <wesley.araujo@isgsa.com.br>
2020-11-07 16:09:41 -0300

    Removido insert nao usado

--- ZSSN/src/main/resources/db/migration/h2/V2__insert_sample_data.sql
+++ ZSSN/src/main/resources/db/migration/h2/V2__insert_sample_data.sql
@@ -1,9 +1,3 @@
-
-INSERT INTO users (email, name, created_at) VALUES
-('admin@gmail.com', 'Admin', CURRENT_TIMESTAMP()),
-('siva@gmail.com', 'Siva', CURRENT_TIMESTAMP())
-;
-
 INSERT INTO survivors (name, birthdate, gender, last_latitude, last_longitude) VALUES
 ('Wesley',to_date('1989-08-14','yyyy-MM-dd'), 'M', -5.08921, -42.8016);
 
