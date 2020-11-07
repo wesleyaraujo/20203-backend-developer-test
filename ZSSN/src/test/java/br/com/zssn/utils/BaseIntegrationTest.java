2066f5b374f17c04d1b646b78e2e57684497c4f8
Wesley Araujo <wesley.araujo@isgsa.com.br>
2020-11-06 22:36:54 -0300

    Ajustes para os testes na base local

--- ZSSN/src/test/java/br/com/zssn/utils/BaseIntegrationTest.java
+++ ZSSN/src/test/java/br/com/zssn/utils/BaseIntegrationTest.java
@@ -1,5 +1,9 @@
 package br.com.zssn.utils;
 
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+import java.time.Duration;
+
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.util.TestPropertyValues;
@@ -9,33 +13,26 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.containers.PostgreSQLContainer;
 
-import java.time.Duration;
-
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-@ContextConfiguration(initializers = {BaseIntegrationTest.Initializer.class})
+@ContextConfiguration(initializers = { BaseIntegrationTest.Initializer.class })
 public abstract class BaseIntegrationTest {
 
-    public static PostgreSQLContainer postgreSQLContainer =
-            (PostgreSQLContainer) new PostgreSQLContainer("postgres:10.4")
-                    .withDatabaseName("appdb")
-                    .withUsername("siva")
-                    .withPassword("secret")
-                    .withStartupTimeout(Duration.ofSeconds(600));
-    static {
-        postgreSQLContainer.start();
-    }
+	public static PostgreSQLContainer postgreSQLContainer = (PostgreSQLContainer) new PostgreSQLContainer(
+			"postgres:13.0").withDatabaseName("postgres").withUsername("postres").withPassword("postgres")
+					.withStartupTimeout(Duration.ofSeconds(600));
+	static {
+		postgreSQLContainer.start();
+	}
 
-    static class Initializer
-            implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
-            TestPropertyValues.of(
-                    "spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
-                    "spring.datasource.username=" + postgreSQLContainer.getUsername(),
-                    "spring.datasource.password=" + postgreSQLContainer.getPassword()
-            ).applyTo(configurableApplicationContext.getEnvironment());
-        }
-    }
+	static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+		@Override
+		public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+			TestPropertyValues
+					.of("spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
+							"spring.datasource.username=" + postgreSQLContainer.getUsername(),
+							"spring.datasource.password=" + postgreSQLContainer.getPassword())
+					.applyTo(configurableApplicationContext.getEnvironment());
+		}
+	}
 }
