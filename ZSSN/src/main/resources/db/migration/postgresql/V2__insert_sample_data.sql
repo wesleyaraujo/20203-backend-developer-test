24d0659375cd147ec0be540920e1074bc31c4f5d
Wesley Araujo <wesley.araujo@isgsa.com.br>
2020-11-06 22:20:20 -0300

    Adicionado script para inserir dados na tabela de resources

--- ZSSN/src/main/resources/db/migration/postgresql/V2__insert_sample_data.sql
+++ ZSSN/src/main/resources/db/migration/postgresql/V2__insert_sample_data.sql
@@ -1,11 +1,5 @@
-
-INSERT INTO users (email, name, created_at) VALUES
-('admin@gmail.com', 'Admin', current_timestamp),
-('siva@gmail.com', 'Siva', current_timestamp)
-;
-
-INSERT INTO survivors (name, birthdate, gender, last_latitude, last_longitude) VALUES
-('Wesley',to_date('1989-08-14','yyyy-MM-dd'), 'M', -5.08921, -42.8016);
+INSERT INTO survivors (name, birthdate, gender, last_latitude, last_longitude, created_at) VALUES
+('Wesley',to_date('1989-08-14','yyyy-MM-dd'), 'M', -5.08921, -42.8016, current_timestamp);
 
 INSERT INTO resources (name, point) VALUES
 ('Water',4),
