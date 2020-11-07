06ee3a5928a5f2ba01cb82fc6c13ae8de52dcb18
Wesley Araujo <wesley.araujo@isgsa.com.br>
2020-11-07 16:09:05 -0300

    Melhorias nas configurações do swagger

--- ZSSN/src/main/java/br/com/zssn/config/SwaggerConfig.java
+++ ZSSN/src/main/java/br/com/zssn/config/SwaggerConfig.java
@@ -1,8 +1,12 @@
 package br.com.zssn.config;
 
+import static java.util.Collections.emptyList;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -11,28 +15,30 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import static java.util.Collections.emptyList;
-
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-    }
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(apiInfo())
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("br.com.zssn.web.controller"))
+				.paths(PathSelectors.any())
+				.build();
+	}
 
-    private ApiInfo apiInfo() {
-        return new ApiInfo(
-                "MyApp REST API",
-                "MyApp REST API",
-                "1.0",
-                "Terms of service",
-                new Contact("MyAppDevTeam", "http://myapps.com", "support@myapp.com"),
-                "License of API", "API license URL", emptyList());
-    }
+	private ApiInfo apiInfo() {
+		return new ApiInfo("REST API ZSSN", "REST API ZSSN", "1.0", "Terms of service",
+				new Contact("ZSSN", "http://zssn.com", "support@zssn.com"), "License of API", "API license URL",
+				emptyList());
+	}
+	
+	@Override
+	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+	    }
 }
