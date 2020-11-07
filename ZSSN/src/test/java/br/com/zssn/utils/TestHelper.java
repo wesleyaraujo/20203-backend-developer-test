8dc4158b06b48416043ae18185f92279a05f405a
Wesley Araujo <wesley.araujo@isgsa.com.br>
2020-11-06 22:37:12 -0300

    Correção na classe de helper

--- ZSSN/src/test/java/br/com/zssn/utils/TestHelper.java
+++ ZSSN/src/test/java/br/com/zssn/utils/TestHelper.java
@@ -1,28 +1,20 @@
-//package br.com.zssn.utils;
-//
-//import br.com.zssn.entity.User;
-//
-//import java.util.Random;
-//import java.util.UUID;
-//
-//import static java.lang.String.format;
-//
-//public class TestHelper {
-//    public static User buildUser() {
-//        String uuid = UUID.randomUUID().toString();
-//        return User.builder()
-//                .name("name-"+uuid)
-//                .email(format("someone-%s@gmail.com", uuid))
-//                .build();
-//    }
-//
-//    public static User buildUserWithId() {
-//        Random random = new Random();
-//        String uuid = UUID.randomUUID().toString();
-//        return User.builder()
-//                .id(random.nextLong())
-//                .name("name-"+uuid)
-//                .email(format("someone-%s@gmail.com", uuid))
-//                .build();
-//    }
-//}
+package br.com.zssn.utils;
+
+import java.util.Random;
+import java.util.UUID;
+
+import br.com.zssn.entity.Survivor;
+
+public class TestHelper {
+	public static Survivor buildSurvivor() {
+		String uuid = UUID.randomUUID().toString();
+		return Survivor.builder().name("name-" + uuid).build();
+	}
+
+	public static Survivor buildSurvivorWithId() {
+		Random random = new Random();
+		String uuid = UUID.randomUUID().toString();
+		return Survivor.builder().id(random.nextLong()).name("name-" + uuid).build();
+	}
+
+}
