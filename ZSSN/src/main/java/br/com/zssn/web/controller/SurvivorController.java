6a4550b610a024aadcca28e18ef20fcbae11da66
Wesley Araujo <wesley.araujo@isgsa.com.br>
2020-11-06 22:18:07 -0300

    Removidos métodos não necessãrios e adicionados novos métodos

--- ZSSN/src/main/java/br/com/zssn/web/controller/SurvivorController.java
+++ ZSSN/src/main/java/br/com/zssn/web/controller/SurvivorController.java
@@ -1,12 +1,8 @@
 package br.com.zssn.web.controller;
 
-import java.util.List;
-import java.util.Objects;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -18,8 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.zssn.entity.Position;
 import br.com.zssn.entity.Survivor;
 import br.com.zssn.service.SurvivorService;
+import br.com.zssn.util.RequestTrade;
+import br.com.zssn.util.ResultOperation;
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * 
+ * @author Wesley Araujo
+ *
+ */
 @RestController
 @RequestMapping("/api/survivors")
 @Slf4j
@@ -32,22 +35,33 @@ public class SurvivorController {
 		this.survivorService = survivorService;
 	}
 
-	@PostMapping("")
+	@PostMapping("/create")
 	@ResponseStatus(HttpStatus.CREATED)
-	public Survivor createSurvivor(@RequestBody Survivor survivor) {
+	public ResponseEntity<ResultOperation> createSurvivor(@RequestBody Survivor survivor) {
 		log.info("process=create-survivor, name={}", survivor.getName());
-		return survivorService.createSurvivor(survivor);
+		ResultOperation result = survivorService.createSurvivor(survivor);
+		return ResponseEntity.ok(result);
 	}
 
 	@PutMapping("/{id}")
-	@ResponseStatus(code = HttpStatus.OK)
-	public ResponseEntity<Survivor> updateLocation(@PathVariable Long id, @RequestBody Position newPosition) {
-		Survivor updateLocation = survivorService.updateLocation(id, newPosition);
-		if (Objects.nonNull(updateLocation)) {
-			return ResponseEntity.ok(updateLocation);
-		} else {
-			return ResponseEntity.notFound().build();
-		}
+	public ResponseEntity<ResultOperation> updateLocation(@PathVariable Long id, @RequestBody Position newPosition) {
+		log.info("process=update-location");
+		ResultOperation result = survivorService.updateLocation(id, newPosition);
+		return ResponseEntity.ok(result);
+	}
+
+	@PutMapping("/infected/{survivorInfectedId}")
+	public ResponseEntity<ResultOperation> informInfected(@PathVariable Long survivorInfectedId) {
+		log.info("process=inform-infected");
+		ResultOperation result = survivorService.informInfected(survivorInfectedId);
+		return ResponseEntity.ok(result);
+	}
+
+	@PostMapping("/trade")
+	public ResponseEntity<ResultOperation> tradeResources(@RequestBody RequestTrade requestTrade) {
+		log.info("process=trade-resources");
+		ResultOperation result = survivorService.tradeResources(requestTrade.getTradeFrom(), requestTrade.getTradeTo());
+		return ResponseEntity.ok(result);
 	}
 
 }
